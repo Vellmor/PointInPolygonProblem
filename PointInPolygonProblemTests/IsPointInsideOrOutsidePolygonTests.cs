f3181f59d1c13fbe7b97b0534f252a784e15d5dd
Калачиков Максим Павлович <xvelx@rambler.ru>
2019-09-09 16:16:33 +0700

    First test

--- PointInPolygonProblemTests/IsPointInsideOrOutsidePolygonTests.cs
+++ PointInPolygonProblemTests/IsPointInsideOrOutsidePolygonTests.cs
@@ -9,7 +9,19 @@ namespace PointInPolygonProblemTests
         [TestMethod]
         public void PointInsidePolygon()
         {
-
+            // Arrange
+            PointF point1 = new PointF(4F, 2F);
+            PointF[] polygon = new PointF[] {
+                                                new PointF(0F, 0F),
+                                                new PointF(0F, 8F),
+                                                new PointF(4F, 4F),
+                                                new PointF(9F, 7F),
+                                                new PointF(13F, 0F),
+                                            };
+            // Act
+            // Assert
+            bool answer = IsPointInsideOrOutsidePolygon.IsPointInPolygon(polygon, point1);
+            Assert.AreEqual(true, answer);
         }
     }
 }
