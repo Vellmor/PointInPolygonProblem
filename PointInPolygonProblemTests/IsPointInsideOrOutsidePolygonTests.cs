200a516d28db195fc526fdfc2644cfa94f62cdf2
Калачиков Максим Павлович <xvelx@rambler.ru>
2019-09-09 16:20:26 +0700

    Fixed small problem with namespace.

--- PointInPolygonProblemTests/IsPointInsideOrOutsidePolygonTests.cs
+++ PointInPolygonProblemTests/IsPointInsideOrOutsidePolygonTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PointInPolygonProblem;
+using System.Drawing;
 
 namespace PointInPolygonProblemTests
 {
@@ -22,6 +23,24 @@ namespace PointInPolygonProblemTests
             // Assert
             bool answer = IsPointInsideOrOutsidePolygon.IsPointInPolygon(polygon, point1);
             Assert.AreEqual(true, answer);
+        }
+
+        [TestMethod]
+        public void PointOutsidePolygonRight()
+        {
+            // Arrange
+            PointF point1 = new PointF(14F, 2F);
+            PointF[] polygon = new PointF[] {
+                                                new PointF(0F, 0F),
+                                                new PointF(0F, 8F),
+                                                new PointF(4F, 4F),
+                                                new PointF(9F, 7F),
+                                                new PointF(13F, 0F),
+                                            };
+            // Act
+            // Assert
+            bool answer = IsPointInsideOrOutsidePolygon.IsPointInPolygon(polygon, point1);
+            Assert.AreEqual(false, answer);
         }
     }
 }
